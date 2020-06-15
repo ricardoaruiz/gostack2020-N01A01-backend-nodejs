19cfa0a0d11c5da7f1d5c25b31190ca8e4989cfb
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-15 20:59:26 -0300

    Back-end Node.js - Criando e utilizando middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 app.use(express.json());
 
 const projects = [];
+
+// Middleware de log de tempo de execução customizado
+const logRequests = (request, response, next) => {
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel)
+}
+
+// Middleware de validação de id customizado
+const validateProjectId = (request, response, next) => {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID'});
+  }
+  return next();
+};
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 // Mapping a route on app
 app.get('/projects', (request, response) => {  
@@ -51,7 +72,7 @@ app.put('/projects/:id', (request, response) => {
 
 app.delete('/projects/:id', (request, response) => {
   const { id } = request.params;
-  
+
   const projectIndex = projects.findIndex(project => project.id === id);
 
   if (projectIndex < 0 ) {
