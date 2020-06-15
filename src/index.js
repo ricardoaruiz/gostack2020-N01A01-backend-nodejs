4b4480a9787bb0d876aeaf845666b54437aab1e6
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-15 20:41:08 -0300

    Back-end Node.js - Implementando as rotas

--- src/index.js
+++ src/index.js
@@ -1,62 +1,69 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
+
 const app = express();
 app.use(express.json());
 
-// Mapping a route on app
-app.get('/projects', (request, response) => {
-  
-  // Query Params
-  const { title, owner } = request.query
-  console.log(title, owner);
+const projects = [];
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+// Mapping a route on app
+app.get('/projects', (request, response) => {  
+ const { title } = request.query
+
+ const filteredProject = title 
+    ? projects.filter(project => project.title.toLowerCase().includes(title.toLowerCase()))
+    : projects;
+
+    return response.json(filteredProject);
 });
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
 
-  // Body
-  console.log(`O corpo da requisição é: ${JSON.stringify(request.body)}`);
-
-  response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4'
-  ]).status(201)
+  const project = {
+    id: uuid(),
+    title,
+    owner,
+  }
+  projects.push(project);
+  response.json(project).status(201)
 });
 
 app.put('/projects/:id', (request, response) => {
-
-  // Route Params
   const { id } = request.params;
-  console.log(`O id recebido foi ${id}`);
+  
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  response.json([
-    'Projeto 10',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4'
-  ]).status(200)
+  if (projectIndex < 0) {
+    return response.status(404).send();
+  }
+
+  const changedProject = {
+    id,
+    ...request.body
+  }
+
+  projects.splice(projectIndex, 1, changedProject);
+
+  return response.json(changedProject).status(200);
+  
 });
 
 app.delete('/projects/:id', (request, response) => {
-
-  // Route Params
   const { id } = request.params;
-  console.log(`O id recebido foi ${id}`);
+  
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  response.json([
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4'
-  ]).status(200)
+  if (projectIndex < 0 ) {
+    return response.status(404).send();
+  }
+
+  projects.splice(projectIndex, 1);
+  response.status(204).send();
 })
 
 // Start server
-app.listen('3333', () => {
+app.listen('3333', (error) => {
+  console.log('error =====> ', error)
   console.log('Server is listening on 3333');
 });
