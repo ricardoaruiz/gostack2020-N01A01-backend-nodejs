40dc706a2e59ddc5aa5fdfbc1edf3a82a1cad6c4
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-15 19:27:25 -0300

    Back-end Node.js - Exmplo de criação de rotas com diferentes verbos HTTP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
-
 const app = express();
 
+// Mapping a route on app
 app.get('/projects', (request, response) => {
-  return response.json({ message: 'Hello projects' });
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
 });
 
+app.post('/projects', (request, response) => {
+  response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4'
+  ]).status(201)
+});
+
+app.put('/projects/:id', (request, response) => {
+  response.json([
+    'Projeto 10',
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4'
+  ]).status(200)
+});
+
+app.delete('/projects/:id', (request, response) => {
+  response.json([
+    'Projeto 2',
+    'Projeto 3',
+    'Projeto 4'
+  ]).status(200)
+})
+
+// Start server
 app.listen('3333', () => {
   console.log('Server is listening on 3333');
-})
+});
