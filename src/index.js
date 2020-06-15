ff752d724c52ed51ea00b881e34e0ce854af02f5
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-15 19:51:13 -0300

    Back-end Node.js - Utilizando os 3 tipos de parâmetros nas rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // Mapping a route on app
 app.get('/projects', (request, response) => {
+  
+  // Query Params
+  const { title, owner } = request.query
+  console.log(title, owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -11,6 +17,10 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+
+  // Body
+  console.log(`O corpo da requisição é: ${JSON.stringify(request.body)}`);
+
   response.json([
     'Projeto 1',
     'Projeto 2',
@@ -20,6 +30,11 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+
+  // Route Params
+  const { id } = request.params;
+  console.log(`O id recebido foi ${id}`);
+
   response.json([
     'Projeto 10',
     'Projeto 2',
@@ -29,6 +44,11 @@ app.put('/projects/:id', (request, response) => {
 });
 
 app.delete('/projects/:id', (request, response) => {
+
+  // Route Params
+  const { id } = request.params;
+  console.log(`O id recebido foi ${id}`);
+
   response.json([
     'Projeto 2',
     'Projeto 3',
