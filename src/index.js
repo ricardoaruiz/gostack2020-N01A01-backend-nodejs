2a8b5a63830b3b8dde90c0d1bb27c8c0b378859e
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-06-17 20:37:06 -0300

    Configurando CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const projects = [];
 
